73b2546ca5b74c2d5a6dc006f9a7c0d73db8d5d8
jgerminario <jgerminario@gmail.com>
2016-12-05 17:22:32 -0800

    moves feed_fetcher to be an app in spectrum_backend project

--- spectrum_backend/settings.py
+++ spectrum_backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'feed_fetcher',
+    'spectrum_backend.feed_fetcher',
 ]
 
 MIDDLEWARE = [

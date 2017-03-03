16747752a0da6c7854a49d293d5c931d498eaa99
jgerminario <jgerminario@gmail.com>
2017-03-03 09:16:34 -0800

    redirect urls job

---
+++ spectrum_backend/feed_fetcher/management/commands/redirect_urls.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand, CommandError
+from spectrum_backend.feed_fetcher.models import FeedItem
+import urllib
+
+class Command(BaseCommand):
+  def handle(self, *args, **options):
+    query_param_delimiter = "?"
+    feed_items = FeedItem.objects.all()
+    for feed_item in feed_items:
+      try:
+        url = urllib.request.urlopen(feed_item.url).geturl()
+        print(url)
+        feed_item.url = url.split(query_param_delimiter)[0]
+        feed_item.save()
+      except:
+        pass

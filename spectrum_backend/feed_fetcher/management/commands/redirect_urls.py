09a557ebd2deb5656ec37b188a705fc0aca70cb2
jgerminario <jgerminario@gmail.com>
2017-03-19 20:37:18 -0700

    adds catch for http error

--- spectrum_backend/feed_fetcher/management/commands/redirect_urls.py
+++ spectrum_backend/feed_fetcher/management/commands/redirect_urls.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from spectrum_backend.feed_fetcher.models import FeedItem
 from django.core.paginator import Paginator
 from urllib import parse
+from urllib.error import HTTPError
 import urllib
 
 class Command(BaseCommand):
@@ -10,7 +11,11 @@ class Command(BaseCommand):
     for page in range(1, paginator.num_pages + 1):
       print("Parsing next 10000 URLs")
       for feed_item in paginator.page(page).object_list:
-        url = urllib.request.urlopen(feed_item.url).geturl()
-        url_parameter_delimiter = "?"
-        feed_item.redirected_url = url.split(url_parameter_delimiter)[0]
-        feed_item.save()
+        try:
+          url = urllib.request.urlopen(feed_item.url).geturl()
+          url_parameter_delimiter = "?"
+          feed_item.redirected_url = url.split(url_parameter_delimiter)[0]
+          feed_item.save()
+        except HTTPError as e:
+          feed_item.redirected_url = feed_item.url
+          feed_item.save()

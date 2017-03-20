c1ad6d74eadc13f56eb1514a9965cc8b1c0b2384
jgerminario <jgerminario@gmail.com>
2017-03-19 22:58:16 -0700

    increases association

--- spectrum_backend/feed_fetcher/management/commands/seed_base_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/seed_base_associations.py
@@ -2,8 +2,11 @@ from django.core.management.base import BaseCommand, CommandError
 from spectrum_backend.feed_fetcher.models import FeedItem
 from .tfidf import main
 
+DAYS_TO_CHECK_FOR = 21
+
 # Initial seed for all associations
 class Command(BaseCommand):
   def handle(self, *args, **options):
-    feed_items = FeedItem.recent_items_eligible_for_association(4)
+    feed_items = FeedItem.recent_items_eligible_for_association(DAYS_TO_CHECK_FOR)
     main(feed_items)
+    # TODO - delete associations with itself

d73d40aaebcffe5c9abd30bf05ed6fc8d88ec509
jgerminario <jgerminario@gmail.com>
2017-04-10 00:50:22 -0700

    attempts fix at celery job termination

--- spectrum_backend/feed_fetcher/tasks.py
+++ spectrum_backend/feed_fetcher/tasks.py
@@ -1,12 +1,12 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
-from django.core.management import call_command
+from management.commands._add_new_associations import add
 from celery.task import task
 
 logger = get_task_logger(__name__)
 
 @task()
 def task_add_new_associations():
-    call_command('add_new_associations')
+    add()
 

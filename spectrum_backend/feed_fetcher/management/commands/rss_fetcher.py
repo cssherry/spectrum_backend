76a8edd4bf4a731bde006d1e71eae3877a273971
jgerminario <jgerminario@gmail.com>
2017-04-16 17:36:42 -0700

    fixes job

--- spectrum_backend/feed_fetcher/management/commands/rss_fetcher.py
+++ spectrum_backend/feed_fetcher/management/commands/rss_fetcher.py
@@ -1,6 +1,42 @@
 from django.core.management.base import BaseCommand, CommandError
-from ._rss_fetcher import RSSFetcher
+from spectrum_backend.feed_fetcher.models import Feed
+from spectrum_backend.feed_fetcher.models import FeedItem
+from spectrum_backend.feed_fetcher.models import Tag
+from ._rss_entry_wrapper import RSSEntryWrapper
+from ._feed_item_processor import FeedItemProcessor
+import feedparser
+import scrapy
+import os
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from spectrum_backend.feed_fetcher.tasks import task_add_new_associations
+
+ASSOCIATION_MEMORY_THRESHOLD = int(os.environ['ASSOCIATION_MEMORY_THRESHOLD']) or 2000
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
-    RSSFetcher().fetch(self.stdout, self.style)
+    for feed in Feed.objects.all():
+      feed_result = feedparser.parse(feed.rss_url)
+      self.stdout.write(self.style.SUCCESS(self.__parse_message(feed)))
+
+      for entry in feed_result.entries:
+        self.__parse_entry(feed, entry)
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl('articles', memory_threshold=ASSOCIATION_MEMORY_THRESHOLD)
+    process.start()
+    task_add_new_associations.delay()
+
+  def __parse_entry(self, feed, entry):
+    entry_wrapper = RSSEntryWrapper(feed, entry)
+    url_parameter_delimiter = "?"
+    url = entry_wrapper.url.split(url_parameter_delimiter)[0]
+
+    feed_item = FeedItem.objects.get_or_create(url=url, defaults={'feed': feed, 'redirected_url': url, 'title': entry_wrapper.title, 'raw_description': entry_wrapper.raw_description, 'author': entry_wrapper.author, 'image_url': entry_wrapper.image_url, 'publication_date': entry_wrapper.publication_date})[0]
+    feed_item = FeedItemProcessor().process(feed_item)
+    feed_item.save()
+    for tag in entry_wrapper.tags:
+      Tag.objects.get_or_create(name=tag, feed_item=feed_item)
+
+  def __parse_message(self, feed):
+    return 'Parsing items from %s (%s) - %s' % (feed.publication.name, feed.category, feed.rss_url)
--- spectrum_backend/feed_fetcher/tasks.py
+++ spectrum_backend/feed_fetcher/tasks.py
@@ -1,30 +1,11 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
-from spectrum_backend.feed_fetcher.management.commands._add_new_associations import add
-# from spectrum_backend.feed_fetcher.management.commands._rss_fetcher import RSSFetcher
+from django.core.management import call_command
 from celery.task import task
-from celery import Celery
 
 logger = get_task_logger(__name__)
-app = Celery ()
-
-# MINUTES_BETWEEN_FETCHES = int(os.environ['MINUTES_BETWEEN_FETCHES']) or 1
-
-# @on_after_configure.connect()
-#   def setup_periodic_tasks(sender, **kwargs):
-#     sender.add_periodic_task(60*MINUTES_BETWEEN_FETCHES, task_fetch_rss)
-
-# MINUTES_BETWEEN_FETCHES = int(os.environ['MINUTES_BETWEEN_FETCHES']) or 1
-
-# @on_after_configure.connect()
-#   def setup_periodic_tasks(sender, **kwargs):
-#     sender.add_periodic_task(60*MINUTES_BETWEEN_FETCHES, task_fetch_rss)
 
 @task()
 def task_add_new_associations():
-    add()
-
-# @task()
-# def task_fetch_rss():
-#   RSSFetcher.fetch()
+    call_command('add_new_associations')

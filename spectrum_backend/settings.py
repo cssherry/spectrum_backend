74640a6db1e50092f990082e91f385cb3cad405c
jgerminario <jgerminario@gmail.com>
2017-04-18 23:38:34 -0700

    no git sha raven

--- spectrum_backend/settings.py
+++ spectrum_backend/settings.py
@@ -162,5 +162,5 @@ RAVEN_CONFIG = {
     'dsn': os.environ['SPECTRUM_SENTRY_KEY'],
     # If you are using git, you can also automatically configure the
     # release based on the git info.
-    'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
+    # 'release': raven.fetch_git_sha(os.path.dirname(os.pardir)),
 }

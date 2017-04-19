ee6b10a86cea783684beeee024d568313e11ad68
jgerminario <jgerminario@gmail.com>
2017-04-19 00:08:26 -0700

    raven and new relic stuff

--- spectrum_backend/feed_fetcher/management/commands/raven_tester.py
+++ spectrum_backend/feed_fetcher/management/commands/raven_tester.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from raven.contrib.django.raven_compat.models import client
 
 # Adds new associations
 class Command(BaseCommand):

586f08f06532cfff07af37dfa4bbb77e2215ff61
jgerminario <jgerminario@gmail.com>
2017-04-18 22:38:23 -0700

    fix

--- spectrum_backend/feed_fetcher/tasks.py
+++ spectrum_backend/feed_fetcher/tasks.py
@@ -13,24 +13,12 @@ import time
 
 logger = get_task_logger(__name__)
 
-# @app.task
-# def task_fetch_rss():
-#   i = inspect()
-#   for host_name, workers in i.active().iteritems():
-#     if len(workers) < 2:
-#       print "starting rss_fetcher job"
-#       call_command('rss_fetcher')
-#       return
-#     else:
-#       print "fetcher job already active, suppressing new job"
-#       app.control.revoke(task_id)
-#       return
-
 @app.task
-def test_shit():
-  print("hey")
+def task_fetch_rss():
   i = inspect()
   for host_name, workers in i.active().iteritems():
-    print len(workers)
-
-  return
+    if len(workers) < 2:
+      print "starting rss_fetcher job"
+      call_command('rss_fetcher')
+    else:
+      print "fetcher job already active, suppressing new job"

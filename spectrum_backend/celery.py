2d299ba7a9130b09cb40dfd6069a82e352008b5d
jgerminario <jgerminario@gmail.com>
2017-04-19 00:55:18 -0700

    get rid of info level

--- spectrum_backend/celery.py
+++ spectrum_backend/celery.py
@@ -12,7 +12,7 @@ client = Client(os.environ['SPECTRUM_SENTRY_KEY'])
 # The register_logger_signal function can also take an optional argument
 # `loglevel` which is the level used for the handler created.
 # Defaults to `logging.ERROR`
-register_logger_signal(client, loglevel=logging.INFO)
+register_logger_signal(client, loglevel=logging.WARNING)
 
 # hook into the Celery error handler
 register_signal(client)

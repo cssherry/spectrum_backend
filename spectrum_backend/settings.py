41f19a43958868f4a553e63d9e5df41b3e15f732
jgerminario <jgerminario@gmail.com>
2017-04-19 00:23:12 -0700

    adds celery raven tracking and tweaks settings

--- spectrum_backend/settings.py
+++ spectrum_backend/settings.py
@@ -180,7 +180,7 @@ LOGGING = {
     },
     'handlers': {
         'sentry': {
-            'level': 'ERROR', # To capture more than ERROR, change to WARNING, INFO, etc.
+            'level': 'WARNING', # To capture more than ERROR, change to WARNING, INFO, etc.
             'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
             'tags': {'custom-tag': 'x'},
         },

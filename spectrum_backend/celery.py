d4a3c31c260bcf8145bd5d2f549242f8044155b5
jgerminario <jgerminario@gmail.com>
2017-04-19 00:57:55 -0700

    removes warning logging

--- spectrum_backend/celery.py
+++ spectrum_backend/celery.py
@@ -12,7 +12,7 @@ client = Client(os.environ['SPECTRUM_SENTRY_KEY'])
 # The register_logger_signal function can also take an optional argument
 # `loglevel` which is the level used for the handler created.
 # Defaults to `logging.ERROR`
-register_logger_signal(client, loglevel=logging.WARNING)
+register_logger_signal(client)
 
 # hook into the Celery error handler
 register_signal(client)

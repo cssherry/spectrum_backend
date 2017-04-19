5913b4d2f52f788c5a71948f96a28f4818f9cb75
jgerminario <jgerminario@gmail.com>
2017-04-19 00:52:26 -0700

    celery settings

--- spectrum_backend/celery.py
+++ spectrum_backend/celery.py
@@ -9,21 +9,13 @@ from raven.contrib.celery import register_signal, register_logger_signal
 # Sentry
 client = Client(os.environ['SPECTRUM_SENTRY_KEY'])
 
-# register a custom filter to filter out duplicate logs
-register_logger_signal(client)
-
 # The register_logger_signal function can also take an optional argument
 # `loglevel` which is the level used for the handler created.
 # Defaults to `logging.ERROR`
-register_logger_signal(client, loglevel=logging.WARNING)
+register_logger_signal(client, loglevel=logging.INFO)
 
 # hook into the Celery error handler
 register_signal(client)
-
-# The register_signal function can also take an optional argument
-# `ignore_expected` which causes exception classes specified in Task.throws
-# to be ignored
-register_signal(client)# , ignore_expected=True)
 
 try:
   MINUTES_BETWEEN_FETCHES = int(os.environ['MINUTES_BETWEEN_FETCHES']) or 20

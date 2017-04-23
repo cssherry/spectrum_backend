06d05faac9351f3f957fbf6b141faa5251c7f131
jgerminario <jgerminario@gmail.com>
2017-04-23 16:27:52 -0700

    fixes shorten urls method

--- spectrum_backend/feed_fetcher/management/commands/_url_parser.py
+++ spectrum_backend/feed_fetcher/management/commands/_url_parser.py
@@ -2,16 +2,25 @@ import re
 from urlparse import urlparse
 from raven.contrib.django.raven_compat.models import client
 from spectrum_backend.feed_fetcher.models import FeedItem
+from django.db.utils import IntegrityError
 from ._batch_query_set import batch_query_set
 
 class URLParser:
     def batch_shorten_urls(self): # TODO - deprecate after fixing in scraper
         all_feed_items = FeedItem.objects.all()
+        count = 0
         for start, end, total, batched_feed_items in batch_query_set(all_feed_items):
             for feed_item in batched_feed_items:
                 feed_item.lookup_url = self.shorten_url(feed_item.redirected_url)
-                feed_item.save()
+                try:
+                    feed_item.save()
+                except IntegrityError:
+                    feed_item.delete()
+                    count += 1
+
             print "%s items processed out of %s" % (end, total)
+
+        print "%s items deleted" % count
 
     def clean_url(self, raw_url):
         url_parameter_delimiter = "?"

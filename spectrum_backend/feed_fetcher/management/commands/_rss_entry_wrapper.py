06889ecabc6960ccbb9173b5af8c154602d2c6cd
jgerminario <jgerminario@gmail.com>
2017-04-23 02:51:50 -0700

    [WIP] refactor of rss entry wrapper and tag wrapper

--- spectrum_backend/feed_fetcher/management/commands/_rss_entry_wrapper.py
+++ spectrum_backend/feed_fetcher/management/commands/_rss_entry_wrapper.py
@@ -4,63 +4,63 @@ from django.utils import timezone
 from raven.contrib.django.raven_compat.models import client
 
 class RSSEntryWrapper:
-  def __init__(self, feed, entry):
-    self.entry = entry
-    self.feed = feed
-    self.title = self.__parsed_title()
-    self.url = self.__parsed_url()
-    self.raw_description = self.__parsed_description()
-    self.author = self.__parsed_author()
-    self.image_url = self.__parsed_image_url()
-    self.publication_date = self.__parsed_publication_date()
-    self.tags = self.__parsed_tags()
+    def __init__(self, feed, entry):
+        self.entry = entry
+        self.feed = feed
+        self.title = self.__parsed_title()
+        self.url = self.__parsed_url()
+        self.raw_description = self.__parsed_description()
+        self.author = self.__parsed_author()
+        self.image_url = self.__parsed_image_url()
+        self.publication_date = self.__parsed_publication_date()
+        self.tags = self.__parsed_tags()
 
-  def __matches_element_format(self, element_name):
-    return hasattr(self.entry, element_name)
+    def __matches_element_format(self, element_name):
+        return hasattr(self.entry, element_name)
 
-  def __parsed_title(self):
-    if self.__matches_element_format('title'):
-      return self.entry.title
-    else:
-      return ""
+    def __parsed_title(self):
+        if self.__matches_element_format('title'):
+            return self.entry.title
+        else:
+            return ""
 
-  def __parsed_description(self):
-    if self.__matches_element_format('description'):
-      return self.entry.description
-    else:
-      return ""
+    def __parsed_description(self):
+        if self.__matches_element_format('description'):
+            return self.entry.description
+        else:
+            return ""
 
-  def __parsed_author(self):
-    if self.__matches_element_format('author'):
-      return self.entry.author
-    else:
-      return ""
+    def __parsed_author(self):
+        if self.__matches_element_format('author'):
+            return self.entry.author
+        else:
+            return ""
 
-  def __parsed_url(self):
-    if self.__matches_element_format('link'):
-      return self.entry.link
-    else:
-      return ""
+    def __parsed_url(self):
+        if self.__matches_element_format('link'):
+            return self.entry.link
+        else:
+            return ""
 
-  def __parsed_image_url(self):
-    if self.__matches_element_format('media_content') and self.entry.media_content[0]:
-      return self.entry.media_content[0]["url"]
-    else:
-      return ""
+    def __parsed_image_url(self):
+        if self.__matches_element_format('media_content') and self.entry.media_content[0]:
+            return self.entry.media_content[0]["url"]
+        else:
+            return ""
 
-  def __parsed_publication_date(self):
-    try:
-      if self.__matches_element_format('published'):
-        return dateutil.parser.parse(self.entry.published)
-      else:
-        return timezone.now() # TODO: find a better solution to this - maybe URL matching for date? Washington Post is culprit
-    except ValueError:
-      client.captureException()
+    def __parsed_publication_date(self):
+        try:
+            if self.__matches_element_format('published'):
+                return dateutil.parser.parse(self.entry.published) # TODO: May be causing timezone-naive errors
+            else:
+                return timezone.now() # TODO: find a better solution to this - maybe URL matching for date? Washington Post is culprit. Media Matters also has date problems
+        except ValueError:
+            client.captureException()
 
-  def __parsed_tags(self):
-    tags = []
-    if self.__matches_element_format('tags'):
-      for tag in self.entry.tags:
-        tags.append(tag.term)
+    def __parsed_tags(self):
+        tags = []
+        if self.__matches_element_format('tags'):
+            for tag in self.entry.tags:
+                tags.append(tag.term)
 
-    return tags
+        return tags

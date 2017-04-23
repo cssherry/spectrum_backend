32fce709cfe0eebd15a48f4d9bd3398f54039a8b
jgerminario <jgerminario@gmail.com>
2017-04-23 02:50:19 -0700

    [WIP] refactors of association jobs

--- spectrum_backend/feed_fetcher/management/commands/_add_new_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/_add_new_associations.py
@@ -1,28 +1,19 @@
 from spectrum_backend.feed_fetcher.models import FeedItem
+from django.conf import settings
 from .tfidf import main
 import os
 
-try:
-  DAYS_TO_CHECK_FOR = int(os.environ['DAYS_TO_CHECK_FOR']) or 14
-except KeyError:
-  DAYS_TO_CHECK_FOR = 14
+class AddNewAssociations:
+    def add(debug=False):
+        all_relevant_feed_items = FeedItem.recent_items_eligible_for_association(settings.DAYS_TO_CHECK_FOR)
+        already_checked = all_relevant_feed_items.filter(checked_for_associations=True)
+        new_feed_items = all_relevant_feed_items.filter(checked_for_associations=False)
 
-try:
-  ASSOCIATION_MEMORY_THRESHOLD = int(os.environ['ASSOCIATION_MEMORY_THRESHOLD']) or 1000
-except KeyError:
-  ASSOCIATION_MEMORY_THRESHOLD = 1000
+        if debug:
+            already_checked = already_checked[:100]
+            new_feed_items = new_feed_items[:10]
 
-# Adds new associations
-def add(debug=False):
-  all_relevant_feed_items = FeedItem.recent_items_eligible_for_association(DAYS_TO_CHECK_FOR)
-  already_checked = all_relevant_feed_items.filter(checked_for_associations=True)
-  new_feed_items = all_relevant_feed_items.filter(checked_for_associations=False)
-
-  if debug:
-    already_checked = already_checked[:100]
-    new_feed_items = new_feed_items[:10]
-
-  if new_feed_items.count() > 0:
-    main(already_checked, new_feed_items, memory_threshold=ASSOCIATION_MEMORY_THRESHOLD)
-  else:
-    print("No new associations")
+        if new_feed_items.count() > 0:
+            main(already_checked, new_feed_items, memory_threshold=settings.ASSOCIATION_MEMORY_THRESHOLD)
+        else:
+            print("No new associations")
--- spectrum_backend/feed_fetcher/management/commands/add_new_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/add_new_associations.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-from ._add_new_associations import add
+from ._add_new_associations import AddNewAssociations
 
 # Adds new associations
 class Command(BaseCommand):
   def handle(self, *args, **options):
-    add()
+    AddNewAssociations().add()

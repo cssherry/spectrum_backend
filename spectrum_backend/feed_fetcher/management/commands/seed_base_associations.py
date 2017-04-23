8c83eab80891b9c9d870a6aa462bf6427497700a
jgerminario <jgerminario@gmail.com>
2017-04-23 02:50:19 -0700

    [WIP] refactors of association jobs

--- spectrum_backend/feed_fetcher/management/commands/seed_base_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/seed_base_associations.py
@@ -1,13 +1,12 @@
 from django.core.management.base import BaseCommand, CommandError
 from spectrum_backend.feed_fetcher.models import FeedItem
+from django.conf import settings
 from .tfidf import main
 import os
-
-DAYS_TO_CHECK_FOR = int(os.environ['DAYS_TO_CHECK_FOR']) or 14
 
 # Initial seed for all associations
 class Command(BaseCommand):
   def handle(self, *args, **options):
-    feed_items = FeedItem.recent_items_eligible_for_association(DAYS_TO_CHECK_FOR)
+    feed_items = FeedItem.recent_items_eligible_for_association(settings.DAYS_TO_CHECK_FOR)
     main(feed_items)
     # TODO - delete associations with itself

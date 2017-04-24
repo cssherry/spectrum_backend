e5e59f23150826cc6d1104a64a6b069bcba405e5
jgerminario <jgerminario@gmail.com>
2017-04-23 17:23:19 -0700

    [WIP] uniqueness on lookup_url

--- spectrum_backend/feed_fetcher/models.py
+++ spectrum_backend/feed_fetcher/models.py
@@ -91,7 +91,7 @@ class FeedItem(models.Model):
     publication_date = models.DateTimeField()
     redirected_url = models.CharField(max_length=1000, unique=True)
     url = models.CharField(max_length=1000, unique=True)
-    lookup_url = models.CharField(max_length=1000, default="")
+    lookup_url = models.CharField(max_length=1000, unique=True)
     image_url = models.CharField(max_length=1000, default="")
     self_score = models.FloatField(default=0)
     checked_for_associations = models.BooleanField(default=0)

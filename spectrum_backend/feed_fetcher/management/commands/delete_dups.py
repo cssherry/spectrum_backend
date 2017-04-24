8354ce16d4f5a7d5a4a687d18c76f6d5769cc73b
jgerminario <jgerminario@gmail.com>
2017-04-23 17:27:00 -0700

    [WIP] removes unneeded url cleanup code

--- spectrum_backend/feed_fetcher/management/commands/delete_dups.py
+++
@@ -1,7 +0,0 @@
-from django.core.management.base import BaseCommand, CommandError
-from spectrum_backend.feed_fetcher.models import FeedItem
-
-# TODO: delete after migration
-class Command(BaseCommand):
-  def handle(self, *args, **options):
-    FeedItem.delete_duplicate_redirect_urls()

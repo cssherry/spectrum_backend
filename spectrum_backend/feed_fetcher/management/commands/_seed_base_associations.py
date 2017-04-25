7ca2c799ed0fb1ea48a750a6931a5309d6efe570
jgerminario <jgerminario@gmail.com>
2017-04-25 01:28:02 -0700

    out of order lines

--- spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
@@ -10,6 +10,6 @@ class SeedBaseAssociations(BaseCommand):
     def seed(self):
         feed_items = FeedItem.recent_items_eligible_for_association(settings.DAYS_TO_CHECK_FOR)
         main(feed_items)
-        app = heroku_conn.apps()['spectrum-backend']
         heroku_conn = heroku3.from_key(os.environ['SPECTRUM_HEROKU_API_KEY'])
+        app = heroku_conn.apps()['spectrum-backend']
         app.process_formation()['worker'].resize('standard-1x') # We scale down to avoid extra costs

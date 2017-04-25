c65c6581d7d05aa89f8cfd942b8403cf71ef92ba
jgerminario <jgerminario@gmail.com>
2017-04-25 01:23:47 -0700

    correct resize args

--- spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
@@ -10,7 +10,7 @@ class SeedBaseAssociations(BaseCommand):
     def seed(self):
         heroku_conn = heroku3.from_key(os.environ['SPECTRUM_HEROKU_API_KEY'])
         app = heroku_conn.apps()['spectrum-backend']
-        app.process_formation()['worker'].resize(2) # We scale this process up to ensure sufficient memory
+        app.process_formation()['worker'].resize('standard-2x') # We scale this process up to ensure sufficient memory
         feed_items = FeedItem.recent_items_eligible_for_association(settings.DAYS_TO_CHECK_FOR)
         main(feed_items)
-        app.process_formation()['worker'].resize(1) # We scale back down to avoid extra costs
+        app.process_formation()['worker'].resize('standard-1x') # We scale back down to avoid extra costs

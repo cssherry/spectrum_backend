1d67b992d24a2e5445e068cee68cbb2ebcca9333
jgerminario <jgerminario@gmail.com>
2017-04-25 01:27:17 -0700

    add dyno scaling to end

--- spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
@@ -8,9 +8,8 @@ import os
 # Initial seed for all associations
 class SeedBaseAssociations(BaseCommand):
     def seed(self):
-        heroku_conn = heroku3.from_key(os.environ['SPECTRUM_HEROKU_API_KEY'])
-        app = heroku_conn.apps()['spectrum-backend']
-        app.process_formation()['worker'].resize('standard-2x') # We scale this process up to ensure sufficient memory
         feed_items = FeedItem.recent_items_eligible_for_association(settings.DAYS_TO_CHECK_FOR)
         main(feed_items)
-        app.process_formation()['worker'].resize('standard-1x') # We scale back down to avoid extra costs
+        app = heroku_conn.apps()['spectrum-backend']
+        heroku_conn = heroku3.from_key(os.environ['SPECTRUM_HEROKU_API_KEY'])
+        app.process_formation()['worker'].resize('standard-1herokx') # We scale down to avoid extra costs

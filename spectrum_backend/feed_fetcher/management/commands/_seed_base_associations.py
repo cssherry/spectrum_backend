36218f3987573dc234cb3d65ffb5efc2bcc13065
jgerminario <jgerminario@gmail.com>
2017-04-25 01:27:36 -0700

    weird typo

--- spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
+++ spectrum_backend/feed_fetcher/management/commands/_seed_base_associations.py
@@ -12,4 +12,4 @@ class SeedBaseAssociations(BaseCommand):
         main(feed_items)
         app = heroku_conn.apps()['spectrum-backend']
         heroku_conn = heroku3.from_key(os.environ['SPECTRUM_HEROKU_API_KEY'])
-        app.process_formation()['worker'].resize('standard-1herokx') # We scale down to avoid extra costs
+        app.process_formation()['worker'].resize('standard-1x') # We scale down to avoid extra costs

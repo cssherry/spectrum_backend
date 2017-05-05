938fe6ddf02069e2fcca7d1a899823a18e967ea7
jgerminario <jgerminario@gmail.com>
2017-05-05 02:20:29 -0700

    removes dependencies on tfidf test

--- spectrum_backend/feed_fetcher/tests/test_tfidf.py
+++ spectrum_backend/feed_fetcher/tests/test_tfidf.py
@@ -1,21 +1,9 @@
-import sys, os, mock, dateutil, feedparser
+import mock
 from mock import patch, Mock
-from datetime import datetime, timedelta
-from io import StringIO
-from django.utils import timezone
 from django.test import TestCase
 from django.conf import settings
-from django.core.management import call_command
-from django.db.utils import IntegrityError
-from . import factories
 from spectrum_backend.feed_fetcher.models import Publication, Feed, FeedItem, Tag, Association, ScrapyLogItem, CorpusWordFrequency
-from spectrum_backend.feed_fetcher.management.commands import _url_parser, _batch_query_set, _rss_fetcher, _clean_entries, _feed_item_processor, _html_parser, _add_new_associations, seed_base_associations, _rss_entry_wrapper
-from article_scraper.spiders import article_spider
-import article_scraper.settings
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-import logging
-import json
+from spectrum_backend.feed_fetcher.management.commands import tfidf
 
 class TFIDFTestCase(TestCase):
     fixtures = ['publications', 'feeds', 'ten_random_feed_items'] # Just 10 random feed_items
